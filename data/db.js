55180ee6a3e25f382268e84616d9cb615123403b
Henry Neal <henry.neal0@gmail.com>
2019-02-14 13:34:40 -0500

    updated json start

--- data/db.js
+++ data/db.js
@@ -2,4 +2,7 @@ const knex = require('knex');
 
 const config = require('../knexfile.js').development;
 
+const db = process.env.DB || "development";
+const config = require('../knexfile.js')[db];
+
 module.exports = knex(config);
